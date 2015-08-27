2d6284a90da48426c4c32c08f6396d0c418ccf0e
BlackFire_S <bitbuck2q>
2015-08-27 16:17:36 +0300

    Исправлена настройка в Yii, указывающая на корневую папку

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -1,6 +1,8 @@
 <?php
 $params = array_merge(require(__DIR__.'/../../common/config/params.php'), require(__DIR__.'/../../common/config/params-local.php'), require(__DIR__.'/params.php'),
     require(__DIR__.'/params-local.php'));
+use \yii\web\Request;
+$baseUrl = str_replace('/frontend/web', '', (new Request)->getBaseUrl());
 return [
     'id'                  => 'app-frontend',
     'name'                => 'Школа бортпроводников',
@@ -11,7 +13,7 @@ return [
     'controllerNamespace' => 'frontend\controllers',
     'components'          => [
         'request'      => [
-            'baseUrl' => '/',
+            'baseUrl' => $baseUrl,
         ],
         'user'         => [
             'identityClass'   => 'frontend\models\User',
@@ -34,7 +36,7 @@ return [
             'errorAction' => 'site/error',
         ],
         'urlManager'   => [
-            'baseUrl'         => '/',
+            'baseUrl'         => $baseUrl,
             'enablePrettyUrl' => true,
             'showScriptName'  => false,
             'rules'           => [
