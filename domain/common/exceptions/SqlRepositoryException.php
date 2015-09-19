6d60daf3a07414d07dbeea6fed0105dfa43a5510
Oleg Prihodko <shurup@e-mind.ru>
2015-09-19 18:17:51 +0300

    Restore file.

---
+++ domain/common/exceptions/SqlRepositoryException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: Oleg Prihodko
+ * Mail: shuru@e-mind.ru
+ * Date: 30.08.2015
+ * Time: 22:20
+ */
+namespace domain\common\exceptions;
+
+use Exception;
+
+class SqlRepositoryException extends Exception
+{
+
+}
